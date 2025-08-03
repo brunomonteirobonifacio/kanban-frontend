252e0e8c0f9c8daa2e45b094ba758aac76013e7d
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-08-03 13:31:13 -0300

    Criado novo componente para o card da tarefa

---
+++ src/app/kanban-task-card/kanban-task-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-task-card',
+  imports: [],
+  templateUrl: './kanban-task-card.component.html',
+  styleUrl: './kanban-task-card.component.css'
+})
+export class KanbanTaskCardComponent {
+
+}
