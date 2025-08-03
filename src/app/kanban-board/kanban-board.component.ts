55cd93e3629954b8e61a22f63bbd207fe9629fe4
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-08-03 13:32:24 -0300

    Alterado kanban-board para usar o novo componente kanban-task-card, e removida classe relacionada ao card de kanban-board.component.css

--- src/app/kanban-board/kanban-board.component.ts
+++ src/app/kanban-board/kanban-board.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { KanbanTaskCardComponent } from '../kanban-task-card/kanban-task-card.component';
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [],
+  imports: [KanbanTaskCardComponent],
   templateUrl: './kanban-board.component.html',
   styleUrl: './kanban-board.component.css'
 })
