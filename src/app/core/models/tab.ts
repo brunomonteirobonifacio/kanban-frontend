7a3607eb4d67ebd22c08011bba6f12819c6afdfb
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-08-03 15:28:50 -0300

    Adicionada propriedade cards em tab.ts para comportar os cards de tarefas da Tab

--- src/app/core/models/tab.ts
+++ src/app/core/models/tab.ts
@@ -1,4 +1,5 @@
 import { ActionOnMove } from './enums/action-on-move';
+import { Card } from './card';
 
 export class Tab {
   // ACEITA NULL TEMPORARIAMENTE PRA FACILITAR OS TESTES
@@ -6,13 +7,15 @@ export class Tab {
   userId: number | null;
   name: string;
   color: string;
+  cards: Card[];
   actionOnMove: ActionOnMove | null;
 
-  constructor(id: number, userId: number | null, name: string, color: string, actionOnMove: ActionOnMove | null) {
+  constructor(id: number, userId: number, name: string, color: string, cards: Card[] = [], actionOnMove: ActionOnMove | null = null) {
     this.id = id;
     this.userId = userId;
     this.name = name;
     this.color = color;
+    this.cards = cards;
     this.actionOnMove = actionOnMove;
   }
 }
