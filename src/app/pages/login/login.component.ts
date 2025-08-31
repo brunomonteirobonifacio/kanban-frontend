4752255d6b62b3c3d779c63539c5ef258aab4180
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-08-31 17:48:27 -0300

    Iniciada construção do formulário à tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormField, MatLabel, MatInput, MatButtonModule, MatCard, MatCardTitle, MatCardContent, MatCardActions],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
